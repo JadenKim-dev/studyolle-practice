13b6fcaf154b1c3f64e58e99157421344138e0c5
JadenKim-dev <jaden.kim5179@gmail.com>
2023-05-07 19:33:15 +0900

    refactor(SignUp): 회원가입 관련 로직 테스트 코드 작성 및 리팩토링
    - 회원 가입에 대한 테스트 코드 작성
    - 새로운 계정과 관련된 로직을 처리하는 메서드를 분리하고, 해당 역할을 AccountService에 위임

--- src/main/java/com/studyolle/studyollejaden/account/AccountController.java
+++ src/main/java/com/studyolle/studyollejaden/account/AccountController.java
@@ -18,8 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -38,25 +37,8 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO 비밀번호 encoding
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyCreatedByWeb(true)
-                .build();
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디 올레, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
-                + "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
+        accountService.processNewAccount(signUpForm);
 
         return "redirect:/";
     }
-
 }
---
+++ src/main/java/com/studyolle/studyollejaden/account/AccountService.java
@@ -0,0 +1,43 @@
+package com.studyolle.studyollejaden.account;
+
+import com.studyolle.studyollejaden.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    public void processNewAccount(SignUpForm signUpForm) {
+        Account newAccount = saveNewAccount(signUpForm);
+        newAccount.generateEmailCheckToken();
+        sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO 비밀번호 encoding
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyCreatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+        return newAccount;
+    }
+
+    private void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디 올레, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
+                + "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+    }
+}
