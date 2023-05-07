477307dbf1cc1228d6eb2290811b9cb2b49c3752
JadenKim-dev <jaden.kim5179@gmail.com>
2023-05-07 14:13:07 +0900

    feat(App): 기본 프로젝트 설정 및 Security 설정
    - 기본 프로젝트 세팅
    - Security 관련 설정
    - 회원 가입 관련 API 개발
    - 회원가입 뷰 템플릿 틀 작성
    - 회원가입에 대한 테스트 작성

---
+++ src/test/java/com/studyolle/studyollejaden/StudyolleJadenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyolle.studyollejaden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleJadenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
